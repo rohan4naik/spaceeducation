6e4b27bc0bb715a3324444386f9c15b520c5253f
pii <132398624+rohan4naik@users.noreply.github.com>
2025-05-06 00:31:06 +0530

    Add files via upload

---
+++ space.js
@@ -0,0 +1,550 @@
+onEvent("getstarted", "click", function( ) {
+  setScreen("3optionscreen");
+});
+onEvent("quiz", "click", function( ) {
+  setScreen("4quizscreen");
+});
+onEvent("quiz1", "click", function( ) {
+  setScreen("quizstartscreen");
+});
+onEvent("quizstartbutton", "click", function( ) {
+  setScreen("quiz1_1");
+});
+onEvent("quiz1_q1_1", "click", function( ) {
+  showElement("button2");
+  hideElement("button1");
+});
+onEvent("quiz1_q1_2", "click", function( ) {
+  showElement("button1");
+  hideElement("button2");
+});
+onEvent("quiz1_q2_1", "click", function( ) {
+  showElement("button4");
+  hideElement("button3");
+});
+onEvent("quiz1_q2_2", "click", function( ) {
+  showElement("button3");
+  hideElement("button4");
+});
+onEvent("quiz1_next1", "click", function( ) {
+  setScreen("quiz1_2");
+});
+onEvent("quiz1_next2", "click", function( ) {
+  setScreen("quiz1_3");
+});
+onEvent("quiz1_q3_1", "click", function( ) {
+  showElement("button6");
+  hideElement("button5");
+});
+onEvent("quiz1_q3_2", "click", function( ) {
+  showElement("button5");
+  hideElement("button6");
+});
+onEvent("quiz1_q4_1", "click", function( ) {
+  showElement("button7");
+  hideElement("button8");
+});
+onEvent("quiz1_q4_2", "click", function( ) {
+  showElement("button8");
+  hideElement("button7");
+});
+onEvent("quiz1_q5_1", "click", function( ) {
+  showElement("button9");
+  hideElement("button10");
+});
+onEvent("quiz1_q5_2", "click", function( ) {
+  showElement("button10");
+  hideElement("button9");
+});
+onEvent("quiz1_q6_1", "click", function( ) {
+  showElement("button12");
+  hideElement("button11");
+});
+onEvent("quiz1_q6_2", "click", function( ) {
+  showElement("button11");
+  hideElement("button12");
+});
+onEvent("quiz2_q7_1", "click", function( ) {
+  showElement("button13");
+  hideElement("button14");
+});
+onEvent("quiz2_q7_2", "click", function( ) {
+  showElement("button14");
+  hideElement("button13");
+});
+onEvent("quiz2_q8_1", "click", function( ) {
+  showElement("button16");
+  hideElement("button15");
+});
+onEvent("quiz2_q8_2", "click", function( ) {
+  showElement("button15");
+  hideElement("button16");
+});
+onEvent("quiz1_next3", "click", function( ) {
+  setScreen("quiz2_1");
+});
+onEvent("quiz2_next1", "click", function( ) {
+  setScreen("quiz2_2");
+});
+onEvent("button19", "click", function( ) {
+  showElement("q9true");
+  hideElement("q9false");
+});
+onEvent("button20", "click", function( ) {
+  showElement("q9false");
+  hideElement("q9true");
+});
+onEvent("button21", "click", function( ) {
+  showElement("q10true");
+  hideElement("q10false");
+});
+onEvent("button22", "click", function( ) {
+  showElement("q10false");
+  hideElement("q10true");
+});
+onEvent("button25", "click", function( ) {
+  setScreen("quiz2_3");
+});
+onEvent("button28", "click", function( ) {
+  showElement("q11true");
+  hideElement("q11false");
+});
+onEvent("button29", "click", function( ) {
+  showElement("q11false");
+  hideElement("q11true");
+});
+onEvent("button32", "click", function( ) {
+  showElement("q12false");
+  hideElement("q12true");
+});
+onEvent("button33", "click", function( ) {
+  showElement("q12true");
+  hideElement("q12false");
+});
+onEvent("2_3to3_1", "click", function( ) {
+  setScreen("4quizscreen");
+});
+onEvent("quiz2", "click", function( ) {
+  setScreen("quiz3_1");
+});
+onEvent("button37", "click", function( ) {
+  showElement("quiz3q1false");
+  hideElement("quiz3q1true");
+});
+onEvent("button38", "click", function( ) {
+  showElement("quiz3q1true");
+  hideElement("quiz3q1false");
+});
+onEvent("button41", "click", function( ) {
+  showElement("quiz3q2false");
+  hideElement("quiz3q2true");
+});
+onEvent("button42", "click", function( ) {
+  showElement("quiz3q2true");
+  hideElement("quiz3q2false");
+});
+onEvent("button43", "click", function( ) {
+  setScreen("quiz3_2");
+});
+onEvent("button46", "click", function( ) {
+  showElement("button49");
+  hideElement("button48");
+});
+onEvent("button47", "click", function( ) {
+  showElement("button48");
+  hideElement("button49");
+});
+onEvent("button50", "click", function( ) {
+  showElement("button45");
+  hideElement("button44");
+});
+onEvent("button51", "click", function( ) {
+  showElement("button44");
+  hideElement("button45");
+});
+onEvent("button52", "click", function( ) {
+  setScreen("quiz3_3");
+});
+onEvent("button23", "click", function( ) {
+  showElement("button27");
+  hideElement("button26");
+});
+onEvent("button24", "click", function( ) {
+  showElement("button26");
+  hideElement("button27");
+});
+onEvent("button30", "click", function( ) {
+  showElement("button18");
+  hideElement("button17");
+});
+onEvent("button31", "click", function( ) {
+  showElement("button17");
+  hideElement("button18");
+});
+onEvent("button34", "click", function( ) {
+  setScreen("quiz4_1");
+});
+onEvent("button39", "click", function( ) {
+  showElement("button54");
+  hideElement("button53");
+});
+onEvent("button40", "click", function( ) {
+  showElement("button53");
+  hideElement("button54");
+});
+onEvent("button55", "click", function( ) {
+  showElement("button36");
+  hideElement("button35");
+});
+onEvent("button56", "click", function( ) {
+  showElement("button35");
+  hideElement("button36");
+});
+onEvent("button57", "click", function( ) {
+  setScreen("quiz4_2");
+});
+onEvent("button60", "click", function( ) {
+  showElement("button62");
+  hideElement("button63");
+});
+onEvent("button61", "click", function( ) {
+  showElement("button63");
+  hideElement("button62");
+});
+onEvent("button64", "click", function( ) {
+  showElement("button59");
+  hideElement("button58");
+});
+onEvent("button65", "click", function( ) {
+  showElement("button58");
+  hideElement("button59");
+});
+onEvent("button66", "click", function( ) {
+  setScreen("quiz4_3");
+});
+onEvent("button69", "click", function( ) {
+  showElement("button71");
+  hideElement("button72");
+});
+onEvent("button70", "click", function( ) {
+  showElement("button72");
+  hideElement("button71");
+});
+onEvent("button73", "click", function( ) {
+  showElement("button68");
+  hideElement("button67");
+});
+onEvent("button74", "click", function( ) {
+  showElement("button67");
+  hideElement("button68");
+});
+onEvent("button75", "click", function( ) {
+  setScreen("4quizscreen");
+});
+onEvent("button78", "click", function( ) {
+  showElement("button80");
+  hideElement("button81");
+});
+onEvent("button79", "click", function( ) {
+  showElement("button81");
+  hideElement("button80");
+});
+onEvent("button82", "click", function( ) {
+  showElement("button76");
+  hideElement("button77");
+});
+onEvent("button83", "click", function( ) {
+  showElement("button77");
+  hideElement("button76");
+});
+onEvent("button84", "click", function( ) {
+  setScreen("quiz5_2");
+});
+onEvent("button88", "click", function( ) {
+  showElement("button91");
+  hideElement("button90");
+});
+onEvent("button89", "click", function( ) {
+  showElement("button90");
+  hideElement("button91");
+});
+onEvent("button92", "click", function( ) {
+  showElement("button87");
+  hideElement("button86");
+});
+onEvent("button93", "click", function( ) {
+  showElement("button86");
+  hideElement("button87");
+});
+onEvent("button94", "click", function( ) {
+  setScreen("quiz5_3");
+});
+
+onEvent("button97", "click", function( ) {
+  showElement("button99");
+  hideElement("button100");
+});
+onEvent("button98", "click", function( ) {
+  showElement("button100");
+  hideElement("button99");
+});
+onEvent("button101", "click", function( ) {
+  showElement("button96");
+  hideElement("button95");
+});
+onEvent("button102", "click", function( ) {
+  showElement("button95");
+  hideElement("button96");
+});
+onEvent("button103", "click", function( ) {
+  setScreen("quiz6_1");
+});
+
+onEvent("button106", "click", function( ) {
+  showElement("button108");
+  hideElement("button109");
+});
+onEvent("button107", "click", function( ) {
+  showElement("button109");
+  hideElement("button108");
+});
+onEvent("button110", "click", function( ) {
+  showElement("button104");
+  hideElement("button105");
+});
+onEvent("button111", "click", function( ) {
+  showElement("button105");
+  hideElement("button104");
+});
+onEvent("button112", "click", function( ) {
+  setScreen("quiz6_2");
+});
+
+onEvent("button115", "click", function( ) {
+  showElement("button118");
+  hideElement("button117");
+});
+onEvent("button116", "click", function( ) {
+  showElement("button117");
+  hideElement("button118");
+});
+onEvent("button119", "click", function( ) {
+  showElement("button114");
+  hideElement("button113");
+});
+onEvent("button120", "click", function( ) {
+  showElement("button113");
+  hideElement("button114");
+});
+onEvent("button121", "click", function( ) {
+  setScreen("quiz6_3");
+});
+
+onEvent("button124", "click", function( ) {
+  showElement("button127");
+  hideElement("button126");
+});
+onEvent("button125", "click", function( ) {
+  showElement("button126");
+  hideElement("button127");
+});
+onEvent("button128", "click", function( ) {
+  showElement("button123");
+  hideElement("button122");
+});
+onEvent("button129", "click", function( ) {
+  showElement("button122");
+  hideElement("button123");
+});
+onEvent("button130", "click", function( ) {
+  setScreen("4quizscreen");
+});
+onEvent("quiz3", "click", function( ) {
+  setScreen("quiz5_1");
+});
+
+
+onEvent("button133", "click", function( ) {
+  showElement("button135");
+  hideElement("button136");
+});
+onEvent("button134", "click", function( ) {
+  showElement("button136");
+  hideElement("button135");
+});
+onEvent("button137", "click", function( ) {
+  showElement("button132");
+  hideElement("button131");
+});
+onEvent("button138", "click", function( ) {
+  showElement("button131");
+  hideElement("button132");
+});
+onEvent("button139", "click", function( ) {
+  setScreen("quiz7_2");
+});
+
+
+onEvent("button142", "click", function( ) {
+  showElement("button145");
+  hideElement("button144");
+});
+onEvent("button143", "click", function( ) {
+  showElement("button144");
+  hideElement("button145");
+});
+onEvent("button146", "click", function( ) {
+  showElement("button140");
+  hideElement("button141");
+});
+onEvent("button147", "click", function( ) {
+  showElement("button141");
+  hideElement("button140");
+});
+onEvent("button148", "click", function( ) {
+  setScreen("quiz7_3");
+});
+
+onEvent("button151", "click", function( ) {
+  showElement("button154");
+  hideElement("button153");
+});
+onEvent("button152", "click", function( ) {
+  showElement("button153");
+  hideElement("button154");
+});
+onEvent("button155", "click", function( ) {
+  showElement("button150");
+  hideElement("button149");
+});
+onEvent("button158", "click", function( ) {
+  showElement("button149");
+  hideElement("button150");
+});
+onEvent("button148", "click", function( ) {
+  setScreen("quiz7_3");
+});
+onEvent("quiz4", "click", function( ) {
+  setScreen("quiz7_1");
+});
+
+
+onEvent("button161", "click", function( ) {
+  showElement("button164");
+  hideElement("button163");
+});
+onEvent("button162", "click", function( ) {
+  showElement("button163");
+  hideElement("button164");
+});
+onEvent("button165", "click", function( ) {
+  showElement("button160");
+  hideElement("button159");
+});
+onEvent("button166", "click", function( ) {
+  showElement("button159");
+  hideElement("button160");
+});
+onEvent("button167", "click", function( ) {
+  setScreen("quiz8_2");
+});
+
+
+onEvent("button170", "click", function( ) {
+  showElement("button173");
+  hideElement("button172");
+});
+onEvent("button171", "click", function( ) {
+  showElement("button172");
+  hideElement("button173");
+});
+onEvent("button174", "click", function( ) {
+  showElement("button168");
+  hideElement("button169");
+});
+onEvent("button175", "click", function( ) {
+  showElement("button169");
+  hideElement("button168");
+});
+onEvent("button167", "click", function( ) {
+  setScreen("quiz8_2");
+});
+
+
+onEvent("button170", "click", function( ) {
+  showElement("button173");
+  hideElement("button172");
+});
+onEvent("button171", "click", function( ) {
+  showElement("button172");
+  hideElement("button173");
+});
+onEvent("button174", "click", function( ) {
+  showElement("button168");
+  hideElement("button169");
+});
+onEvent("button175", "click", function( ) {
+  showElement("button169");
+  hideElement("button168");
+});
+onEvent("button167", "click", function( ) {
+  setScreen("quiz8_3");
+});
+onEvent("button179", "click", function( ) {
+  showElement("button182");
+  hideElement("button181");
+});
+onEvent("button180", "click", function( ) {
+  showElement("button181");
+  hideElement("button182");
+});
+onEvent("button183", "click", function( ) {
+  showElement("button178");
+  hideElement("button177");
+});
+onEvent("button184", "click", function( ) {
+  showElement("button177");
+  hideElement("button178");
+});
+onEvent("button167", "click", function( ) {
+  setScreen("4quizscreen");
+});
+onEvent("button157", "click", function( ) {
+  setScreen("quiz8_1");
+});
+onEvent("button167", "click", function( ) {
+  setScreen("quiz8_2");
+});
+onEvent("button176", "click", function( ) {
+  setScreen("quiz8_3");
+});
+onEvent("button185", "click", function( ) {
+  setScreen("3optionscreen");
+});
+onEvent("storylink", "click", function( ) {
+  open("https://1drv.ms/p/s!AvX5ueawhP1QhyMXEteG1naVOgSv?e=QCaz1T");
+});
+onEvent("button156", "click", function( ) {
+  open("https://youtu.be/cxrLRbkOwKs?feature=shared");
+});
+onEvent("button186", "click", function( ) {
+  open("https://youtu.be/asO2wBT9W9M?feature=shared");
+});
+onEvent("button187", "click", function( ) {
+  open("https://youtu.be/wz01pTvuMa0?feature=shared");
+});
+onEvent("button188", "click", function( ) {
+  open("https://youtu.be/LawlM936TnE?feature=shared");
+});
+onEvent("button189", "click", function( ) {
+  open("https://youtu.be/gSU7NdOPv0w?feature=shared");
+});
+onEvent("button190", "click", function( ) {
+  open("https://youtu.be/n7tnHPDH5d8?feature=shared");
+});
+onEvent("button192", "click", function( ) {
+  open("https://youtu.be/1fOfDH3GCsU?feature=shared");
+});
+onEvent("videolink", "click", function( ) {
+  setScreen("videoslink");
+});
